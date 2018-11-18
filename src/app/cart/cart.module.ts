fc5e9e6c5aa689b518db6ef751501bcc37698fd9
Natalia Mazurczak <natalia.mazurczak@o2.pl>
2018-11-18 02:17:54 +0100

    Add styles

--- src/app/cart/cart.module.ts
+++ src/app/cart/cart.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './cart.component';
 import { ItemComponent } from './item/item.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AngularFontAwesomeModule
   ],
   exports: [
     CartComponent
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,12 +3,15 @@ import { ProductsComponent } from './products.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { SortComponent } from './sort/sort.component';
 import { ProductComponent } from './product/product.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFontAwesomeModule
   ],
   exports: [
+    AngularFontAwesomeModule,
     ProductsComponent,
     ProductComponent
   ],
